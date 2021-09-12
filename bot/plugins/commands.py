c9552d32a34db8b4177025384ee4e0e30e90b304
Showcase Boss <89599166+AdwaithSp@users.noreply.github.com>
2021-09-12 12:28:43 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file name} <br> @mymovieshowcase",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'Series request group', url="https://t.me/series_showcase"
                                 )
                         ]
                     ]
